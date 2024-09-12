8369108884144a4f70cdfcdd9073c850ff347543
Aria <ariari.elk@gmail.com>
2024-09-12 13:39:07 -0500

    Make carrot dissapear when colliding with player

--- carrot.gd
+++ carrot.gd
@@ -5,3 +5,10 @@ extends CharacterBody2D
 func _physics_process(delta):
 	velocity.y += fallSpeed
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider.is_in_group("Player"):
+			print("Player hit")
+			queue_free()
