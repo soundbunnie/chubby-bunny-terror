7ab2451f4dcbceb732df038b26f6ebdddd04d2e2
Aria <ariari.elk@gmail.com>
2024-09-12 13:12:11 -0500

    Add code for spawning carrot

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	var carrotScene = load("res://carrot.tscn")
+	var screenSize = get_viewport().get_visible_rect().size
+	var rand = RandomNumberGenerator.new()
+
+	# Spawn carrot at random position along x axis
+	var carrot = carrotScene.instantiate()
+	rand.randomize()
+	var x = rand.randf_range(0, screenSize.x)
+	carrot.position.x = x
+	add_child(carrot)
