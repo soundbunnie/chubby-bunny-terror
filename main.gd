18d591badfc7a6b21e5deda1d72f6f25442ca703
Aria <ariari.elk@gmail.com>
2025-01-20 03:10:41 -0600

    Change speedmultiplier to be a percentage

--- main.gd
+++ main.gd
@@ -5,6 +5,10 @@ extends Node2D
 @onready var speedLabel = $SpeedLabel
 
 @export var spawnInterval = 0.5
+@export var points_to_progress = 10
+
+var speedMultiplierText = 1
+var speedMultiplier = 0.02
 
 var carrotScene = load("res://carrot.tscn")
 
@@ -15,7 +19,7 @@ func _ready():
 	SignalBus.add_point.connect(_on_add_point)
 	SignalBus.remove_point.connect(_on_remove_point)
 	update_speed_label()
-	update_timer(0)
+	update_timer(0, "adding")
 	spawnTimer.start()
 
 func spawn_carrot():
@@ -33,29 +37,34 @@ func _on_timer_timeout():
 
 func _on_add_point(to_add):
 	score += to_add
-	if score % 10 == 0:
-		update_timer(-0.02)
+	if score % points_to_progress == 0:
+		update_timer(speedMultiplier, "adding")
 		update_speed_label()
 	update_score_label()
 	
 func _on_remove_point(to_remove):
 	if score > 0:
 		score -= to_remove
-		if score % 10 == 0:
-			update_timer(0.02)
+		if score % points_to_progress == 0:
+			update_timer(speedMultiplier, "removing")
 			update_speed_label()
 	update_score_label()
 	
 func update_score_label():
 	scoreLabel.text = "Score: " + str(score)
 	
-func animate_changed_points_label(to_add):
-	pass
-	
 func update_speed_label():
-	speedLabel.text = "Speed: " + str(spawnInterval)
+	speedLabel.text = "Speed: " + str(speedMultiplierText)
 	
-func update_timer(time_to_change):
-	spawnInterval = spawnInterval + time_to_change
+func update_timer(time_to_change, modifier):
+	if modifier == "adding":
+		spawnInterval = spawnInterval + (spawnInterval * time_to_change)
+		speedMultiplierText = speedMultiplierText + (spawnInterval * time_to_change)
+	elif modifier == "removing":
+		spawnInterval = spawnInterval - (spawnInterval * time_to_change)
+		speedMultiplierText = speedMultiplierText - (spawnInterval * time_to_change)
+		
+	spawnInterval = snapped(spawnInterval, 0.01)
+	speedMultiplierText = snapped(speedMultiplierText, 0.01)
 	spawnTimer.wait_time = spawnInterval
 	update_speed_label()
