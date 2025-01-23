d6385071ae1988eef9d5d18c7a290a70b61dde80
Aria <ariari.elk@gmail.com>
2025-01-23 13:57:11 -0600

    Make resume button resume play

--- UI.gd
+++ UI.gd
@@ -6,13 +6,19 @@ var paused = false
 
 func _input(event):
 	if event.is_action_pressed("ui_pause"):
-		if !paused:
-			get_tree().paused = true
-			paused = true
-			pauseScreen.show()
-			SignalBus.pause_game.emit()
-		elif paused:
-			get_tree().paused = false
-			paused = false
-			pauseScreen.hide()
-			SignalBus.unpause_game.emit()
+		pause()
+
+func pause():
+	if !paused:
+		get_tree().paused = true
+		paused = true
+		pauseScreen.show()
+		SignalBus.pause_game.emit()
+	elif paused:
+		get_tree().paused = false
+		paused = false
+		pauseScreen.hide()
+		SignalBus.unpause_game.emit()
+
+func _on_resume_pressed():
+	pause()
