d67036c7ca2cfae86a7fc4eba1c15c4f580c419c
Aria <ariari.elk@gmail.com>
2025-01-23 16:39:06 -0600

    Add pause music

--- scripts/managers/AudioManager.gd
+++ scripts/managers/AudioManager.gd
@@ -4,20 +4,42 @@ extends Node
 @onready var musicPlayer = $MusicPlayer
 @onready var stream = AudioStreamPlayer.new()
 
-var paused_position = 0.0
+var pauseMusic = "Theme of Miranda"
+
+var currentSong
+
+var pausedPosition = 0.0
 
 func _ready():
 	SignalBus.change_music.connect(play_music)
 	SignalBus.change_volume.connect(change_volume)
+	SignalBus.pause_game.connect(play_pause_music)
+	SignalBus.unpause_game.connect(unpause_music)
 	
 func play_music(mus_name):
 	for i in music_array.size():
 		var song = music_array[i]
 		var name = song.resource_path.get_file().get_basename()
 		if name == mus_name:
+			currentSong = name
 			musicPlayer.stream = song
-			musicPlayer.play(paused_position)
-			paused_position = 0.0
+			musicPlayer.play(pausedPosition)
+			pausedPosition = 0.0
+
+func play_pause_music():
+	for i in music_array.size():
+		var song = music_array[i]
+		var name = song.resource_path.get_file().get_basename()
+		if name == pauseMusic:
+			musicPlayer.stream = song
+			musicPlayer.play(0.0)
 
 func change_volume(num):
 	musicPlayer.volume_db = linear_to_db(num)
+
+func pause_music():
+	pausedPosition = musicPlayer.get_playback_position()
+	musicPlayer.stop()
+	
+func unpause_music():
+	play_music(currentSong)
--- scripts/managers/MusicManager.gd
+++ scripts/managers/MusicManager.gd
@@ -1,7 +1,11 @@
 extends Node
 
 @onready var shop_theme_mus = "shop theme wip"
+@onready var theme_of_miranda = "Theme of Miranda"
+
+var current_mus
 
 func _ready():
 	var first_level_music = shop_theme_mus
+	current_mus = first_level_music
 	SignalBus.change_music.emit(first_level_music)
