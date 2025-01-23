126872a034f32c25da23d5ad6906ed89ed6b32a0
Aria <ariari.elk@gmail.com>
2025-01-23 14:06:49 -0600

    Make quit button work

--- UI.gd
+++ UI.gd
@@ -2,7 +2,11 @@ extends Control
 
 @onready var pauseScreen = $PauseScreen
 
+@onready var quitButton = $PauseScreen/PauseMenu/Quit
+
 var paused = false
+
+var quitConfirm = false
 
 func _input(event):
 	if event.is_action_pressed("ui_pause"):
@@ -17,8 +21,17 @@ func pause():
 	elif paused:
 		get_tree().paused = false
 		paused = false
+		quitConfirm = false
+		quitButton.set_text("Quit")
 		pauseScreen.hide()
 		SignalBus.unpause_game.emit()
 
 func _on_resume_pressed():
 	pause()
+
+func _on_quit_pressed():
+	if !quitConfirm:
+		quitButton.set_text("Are you sure?")
+		quitConfirm = true
+	elif quitConfirm:
+		get_tree().quit()
