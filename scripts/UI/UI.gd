6093ad1bbf832e2d0cd351c1ad52d9afdca84bb0
Aria <ariari.elk@gmail.com>
2025-02-02 21:50:36 -0600

    restlye UI.gd

--- scripts/UI/UI.gd
+++ scripts/UI/UI.gd
@@ -1,13 +1,13 @@
 extends Control
 
-@onready var pauseScreen = $PauseScreen
-
-@onready var quitButton = $PauseScreen/PauseMenu/Quit
-
+# private variables
 var paused:bool = false
+var quit_confirm:bool = false
 
-var quitConfirm:bool = false
+# @onready variables
+@onready var pause_screen = $PauseScreen
+@onready var quit_button = $PauseScreen/PauseMenu/Quit
 
 func _input(event):
 	if event.is_action_pressed("ui_pause"):
-		pauseScreen.pause()
+		pause_screen.pause()
