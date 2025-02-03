2f86f482906f79f99097bf7aac1a9e41106a28aa
Aria <ariari.elk@gmail.com>
2025-02-02 21:48:52 -0600

    restyle pause_screen.gd

--- scripts/UI/pause_screen.gd
+++ scripts/UI/pause_screen.gd
@@ -1,20 +1,46 @@
 extends CanvasLayer
 
-@onready var quitButton = $PauseMenu/Quit
-
-@onready var volumeSlider = $PauseMenu/VolumeSlider
-@onready var volumeLabel = $PauseMenu/VolumeSlider/VolumeLabel
-@onready var volumeInput = $"PauseMenu/VolumeSlider/VolumeInput"
-
-@onready var LineEditRegEx := RegEx.new()
-
+#private variables
 var paused:bool = false
+var quit_confirm:bool = false
 
-var quitConfirm:bool = false
+# @onready variables
+@onready var quit_button = $PauseMenu/Quit
+@onready var volume_slider = $PauseMenu/VolumeSlider
+@onready var volume_label = $PauseMenu/VolumeSlider/VolumeLabel
+@onready var volume_input = $"PauseMenu/VolumeSlider/VolumeInput"
+@onready var LineEditRegEx := RegEx.new()
 
 func _ready():
 	LineEditRegEx.compile("^[0-9.]*$")
-	volumeInput.placeholder_text = (str(volumeSlider.value))
+	volume_input.placeholder_text = (str(volume_slider.value))
+
+func _on_resume_pressed():
+	pause()
+
+func _on_quit_pressed():
+	if !quit_confirm:
+		quit_button.set_text("Are you sure?")
+		quit_confirm = true
+	elif quit_confirm:
+		get_tree().quit()
+
+func _on_volume_input_text_submitted(_new_text):
+	volume_input.release_focus()
+	volume_input.text = ""
+
+func _on_volume_input_text_changed(new_text):
+	var old_text = ""
+	if LineEditRegEx.search(new_text) && int(new_text) <= 100:
+		old_text = str(new_text)
+		volume_slider.value = int(new_text)
+	else:
+		volume_input.text = old_text
+		volume_input.set_caret_column(volume_input.text.length())
+
+func _on_volume_slider_value_changed(value):
+	SignalBus.change_volume.emit(value)
+	volume_input.placeholder_text = (str(volume_slider.value))
 
 func pause():
 	if !paused:
@@ -23,34 +49,7 @@ func pause():
 		SignalBus.pause_game.emit()
 	elif paused:
 		paused = false
-		quitConfirm = false
-		quitButton.set_text("Quit")
+		quit_confirm = false
+		quit_button.set_text("Quit")
 		hide()
 		SignalBus.unpause_game.emit()
-
-func _on_resume_pressed():
-	pause()
-
-func _on_quit_pressed():
-	if !quitConfirm:
-		quitButton.set_text("Are you sure?")
-		quitConfirm = true
-	elif quitConfirm:
-		get_tree().quit()
-
-func _on_volume_input_text_submitted(_new_text):
-	volumeInput.release_focus()
-	volumeInput.text = ""
-
-func _on_volume_input_text_changed(new_text):
-	var old_text = ""
-	if LineEditRegEx.search(new_text) && int(new_text) <= 100:
-		old_text = str(new_text)
-		volumeSlider.value = int(new_text)
-	else:
-		volumeInput.text = old_text
-		volumeInput.set_caret_column(volumeInput.text.length())
-
-func _on_volume_slider_value_changed(value):
-	SignalBus.change_volume.emit(value)
-	volumeInput.placeholder_text = (str(volumeSlider.value))
